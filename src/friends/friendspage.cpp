ab25a811fc7ee17ef45043559dd8274a2064bc30
5980Zhongyx <18990007788@189.cn>
2025-11-15 14:24:16 +0800

    feat(capture): add record & publish page + accessibility notes

--- src/friends/friendspage.cpp
+++
@@ -1,9 +0,0 @@
-#include "friendspage.h"
-
-FriendsPage::FriendsPage(QWidget *parent)
-    : QWidget(parent),
-      feedListView(nullptr)
-{
-    // TODO: Initialize UI components
-    // TODO: Load friend videos
-}
