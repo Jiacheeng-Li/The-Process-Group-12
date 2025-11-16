01f231892c170cd82e4dda37838ac6a91fb56fbc
CinnabarRin <873811529@qq.com>
2025-11-16 22:43:32 +0800

    sprint1-ZY-frontend：Initialize frontend, profile page & chat module

--- src/profile/profilepage.cpp
+++
@@ -1,9 +0,0 @@
-#include "profilepage.h"
-
-ProfilePage::ProfilePage(QWidget *parent)
-    : QWidget(parent)
-{
-    // TODO: Initialize profile UI
-    // TODO: Load user information
-    // TODO: Setup editing functionality
-}
