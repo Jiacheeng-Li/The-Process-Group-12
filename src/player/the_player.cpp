a2ddc7def2811c888eed06c0a830bc5995c94875
victoria02020411 <sc23n2c@leeds.ac.uk>
2025-11-20 23:55:34 +0000

    feat(player): add functions to initial player

--- src/player/the_player.cpp
+++ src/player/the_player.cpp
@@ -1,5 +1,5 @@
 //
-//  The Player - Enhanced for Mobile
+//
 //
 
 #include "the_player.h"
@@ -8,24 +8,21 @@
 void ThePlayer::setContent(std::vector<TheButton*>* b, std::vector<TheButtonInfo>* i) {
     buttons = b;
     infos = i;
-    currentIndex = 0;
-    if (buttons->size() > 0) {
-        jumpTo(buttons->at(0)->info);
-    }
+    // 不再自动跳转到第一个视频，在主函数中处理
+    // jumpTo(buttons -> at(0) -> info);
 }
 
 // change the image and video for one button every one second
-// 这个功能现在不再使用，但保留代码以兼容
 void ThePlayer::shuffle() {
-    TheButtonInfo* i = & infos -> at (rand() % infos->size() );
-    buttons -> at( updateCount++ % buttons->size() ) -> init( i );
+    // 不再需要随机切换功能
+    // TheButtonInfo* i = & infos -> at (rand() % infos->size() );
+    // buttons -> at( updateCount++ % buttons->size() ) -> init( i );
 }
 
 void ThePlayer::playStateChanged (QMediaPlayer::State ms) {
     switch (ms) {
         case QMediaPlayer::State::StoppedState:
-            // 视频播放结束后自动播放下一个
-            playNext();
+            play(); // starting playing again...
             break;
     default:
         break;
@@ -35,33 +32,4 @@ void ThePlayer::playStateChanged (QMediaPlayer::State ms) {
 void ThePlayer::jumpTo (TheButtonInfo* button) {
     setMedia( * button -> url);
     play();
-    
-    // 更新当前索引
-    for (size_t i = 0; i < infos->size(); i++) {
-        if (&infos->at(i) == button) {
-            currentIndex = i;
-            break;
-        }
-    }
-}
-
-void ThePlayer::playVideoAtIndex(int index) {
-    if (index >= 0 && index < (int)infos->size()) {
-        currentIndex = index;
-        jumpTo(&infos->at(index));
-    }
-}
-
-void ThePlayer::playNext() {
-    if (infos->size() > 0) {
-        currentIndex = (currentIndex + 1) % infos->size();
-        jumpTo(&infos->at(currentIndex));
-    }
-}
-
-void ThePlayer::playPrevious() {
-    if (infos->size() > 0) {
-        currentIndex = (currentIndex - 1 + infos->size()) % infos->size();
-        jumpTo(&infos->at(currentIndex));
-    }
 }
--- src/player/the_player.h
+++ src/player/the_player.h
@@ -1,10 +1,5 @@
-//
-//  The Player - Enhanced for Mobile
-//
-
 #ifndef CW2_THE_PLAYER_H
 #define CW2_THE_PLAYER_H
-
 
 #include <QApplication>
 #include <QMediaPlayer>
@@ -13,57 +8,46 @@
 #include <QTimer>
 
 class ThePlayer : public QMediaPlayer {
-
-Q_OBJECT
+    Q_OBJECT
 
 private:
     std::vector<TheButtonInfo>* infos;
     std::vector<TheButton*>* buttons;
     QTimer* mTimer;
     long updateCount = 0;
-    int currentIndex = 0;
 
 public:
-    ThePlayer() : QMediaPlayer(NULL) {
-        setVolume(50); // 设置默认音量为50%
-        connect (this, SIGNAL (stateChanged(QMediaPlayer::State)), 
-                this, SLOT (playStateChanged(QMediaPlayer::State)) );
-
-        // 关闭自动切换功能，改为用户手动切换
-        mTimer = new QTimer(NULL);
-        mTimer->setInterval(1000); // 1000ms is one second
-        // 不自动启动 timer，移除自动shuffle功能
-        // mTimer->start();
-        // connect( mTimer, SIGNAL (timeout()), SLOT ( shuffle() ) );
+    ThePlayer(QObject *parent = nullptr) : QMediaPlayer(parent) {
+        setVolume(50);
+        connect(this, SIGNAL(stateChanged(QMediaPlayer::State)), 
+                this, SLOT(playStateChanged(QMediaPlayer::State)));
     }
 
     // all buttons have been setup, store pointers here
     void setContent(std::vector<TheButton*>* b, std::vector<TheButtonInfo>* i);
-    
-    // 获取当前视频索引
-    int getCurrentIndex() const { return currentIndex; }
-    
-    // 播放指定索引的视频
-    void playVideoAtIndex(int index);
-    
-    // 播放下一个视频
-    void playNext();
-    
-    // 播放上一个视频
-    void playPrevious();
-
-private slots:
-
-    // change the image and video for one button every one second
-    // 这个功能现在不再使用
-    void shuffle();
-
-    void playStateChanged (QMediaPlayer::State ms);
 
 public slots:
+    // 添加倍速控制 - 简化版本
+    void setPlaybackRate(qreal rate) {
+        qDebug() << "Setting playback rate to:" << rate;
+        QMediaPlayer::setPlaybackRate(rate);
+        
+        // 检查实际设置的倍速
+        QTimer::singleShot(100, [this, rate]() {
+            qreal actualRate = this->playbackRate();
+            qDebug() << "Actual playback rate:" << actualRate;
+            if (!qFuzzyCompare(actualRate, rate)) {
+                qDebug() << "Warning: Playback rate may not be supported";
+            }
+        });
+    }
 
-    // start playing this ButtonInfo
-    void jumpTo (TheButtonInfo* button);
+private slots:
+    void shuffle();
+    void playStateChanged(QMediaPlayer::State ms);
+
+public slots:
+    void jumpTo(TheButtonInfo* button);
 };
 
-#endif //CW2_THE_PLAYER_H
+#endif // CW2_THE_PLAYER_H
